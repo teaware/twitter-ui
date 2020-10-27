fd146a02a4d23c4533601a306c12075feb2d6428
George Brown <teawares@gmail.com>
2020-10-27 23:02:58 +0800

    Explore Tabs

--- pages/explore.js
+++ pages/explore.js
@@ -1,9 +1,10 @@
 function Explore() {
   return (
     <div className="flex px-4 py-3 space-x-8 border-b border-gray-100">
+      <p className="text-sm font-bold text-gray-500">For you</p>
+      <p className="text-sm font-bold text-gray-500">Trending</p>
+      <p className="text-sm font-bold text-gray-500">News</p>
       <p className="text-sm font-bold text-gray-500">Sports</p>
-      <p className="text-sm font-bold text-gray-500">Fun</p>
-      <p className="text-sm font-bold text-gray-500">Entertainment</p>
     </div>
   );
 }
